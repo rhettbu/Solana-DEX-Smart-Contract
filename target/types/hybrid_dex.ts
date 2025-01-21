4c045dd29da1aeb88ea0e13b0dd8bbdda1ec1737
rhettbu <mafalda32oconnellsfo@hotmail.com>
2025-01-21 15:07:52 +0000

    fix: add deploy key to gitignore

---
+++ target/types/hybrid_dex.ts
@@ -0,0 +1,46 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/hybrid_dex.json`.
+ */
+export type HybridDex = {
+  "address": "6z1NX1CodyGPbJ8sAVirasDsYgw1xSnkyjyprSnMfvRy",
+  "metadata": {
+    "name": "hybridDex",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [],
+      "args": []
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "customError",
+      "msg": "Custom error message"
+    }
+  ],
+  "constants": [
+    {
+      "name": "seed",
+      "type": "string",
+      "value": "\"anchor\""
+    }
+  ]
+};
